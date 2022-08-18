a8ee518a9c90c594e41360de857e7c912036e484
ruru <coco002197@gmail.com>
2022-08-18 10:46:14 +0800

    feat: component style HomeAddTask

---
+++ src/components/Home/HomeAddTask/__tests__/HomeAddTask.spec.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import HomeAddTask from "../HomeAddTask.vue";
+
+describe("HomeAddTask", () => {
+  it("renders properly", () => {
+    const wrapper = mount(HomeAddTask);
+    expect(wrapper.text()).not.toBeNull();
+  });
+});
